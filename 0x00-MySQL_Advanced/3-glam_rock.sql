a862c4ea6503ea8e2a38e5bda25fdde9efdb3fc9
nobleenia <neluwah@gmail.com>
2024-04-25 15:22:16 +0000

    Corrections

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,12 +3,7 @@
 -- ranked by their longevity
 -- Column names must be: band_name & lifespan
 
-SELECT 
-    band_name, 
-    IF(split IS NULL OR split = 0, 2022 - formed, split - formed) AS lifespan
-FROM 
-    metal_bands
-WHERE 
-    style = 'Glam rock'
-ORDER BY 
-    lifespan DESC;
+SELECT band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC
--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,16 +3,14 @@
 -- that computes and stores the average score for a student
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
-BEGIN
-    -- Variable to store the calculated average score
-    DECLARE calculated_avg FLOAT;
-
-    -- Calculate the average score for the given user_id
-    SELECT AVG(score) INTO calculated_avg FROM corrections WHERE user_id = user_id;
-
-    -- Update the user's average_score with the calculated average
-    UPDATE users SET average_score = calculated_avg WHERE id = user_id;
-END$$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser(IN `user_id` INT)
+BEGIN 
+    UPDATE users
+    SET average_score = (SELECT AVG(score)
+                        FROM corrections
+                        WHERE corrections.user_id = user_id)
+    WHERE id = user_id;
+END $$
 
 DELIMITER ;
